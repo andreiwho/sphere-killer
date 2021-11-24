e28e187117684620822e8c179ea1339af676dce3
andreiwho <protsenko.ro.c@gmail.com>
2021-11-25 00:41:34 +0200

    Removed input from SphereAmbushCharacter

--- Source/SphereAmbush/SphereAmbushCharacter.cpp
+++ Source/SphereAmbush/SphereAmbushCharacter.cpp
@@ -17,205 +17,186 @@ DEFINE_LOG_CATEGORY_STATIC(LogFPChar, Warning, All);
 //////////////////////////////////////////////////////////////////////////
 // ASphereAmbushCharacter
 
-ASphereAmbushCharacter::ASphereAmbushCharacter()
-{
-	// Set size for collision capsule
-	GetCapsuleComponent()->InitCapsuleSize(55.f, 96.0f);
+ASphereAmbushCharacter::ASphereAmbushCharacter() {
+    // Set size for collision capsule
+    GetCapsuleComponent()->InitCapsuleSize(55.f, 96.0f);
 
-	// set our turn rates for input
-	BaseTurnRate = 45.f;
-	BaseLookUpRate = 45.f;
+    // set our turn rates for input
+    BaseTurnRate = 45.f;
+    BaseLookUpRate = 45.f;
 
-	// Create a CameraComponent	
-	FirstPersonCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
-	FirstPersonCameraComponent->SetupAttachment(GetCapsuleComponent());
-	FirstPersonCameraComponent->SetRelativeLocation(FVector(-39.56f, 1.75f, 64.f)); // Position the camera
-	FirstPersonCameraComponent->bUsePawnControlRotation = true;
+    // Create a CameraComponent	
+    FirstPersonCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
+    FirstPersonCameraComponent->SetupAttachment(GetCapsuleComponent());
+    FirstPersonCameraComponent->SetRelativeLocation(FVector(-39.56f, 1.75f, 64.f)); // Position the camera
+    FirstPersonCameraComponent->bUsePawnControlRotation = true;
 
-	// Create a mesh component that will be used when being viewed from a '1st person' view (when controlling this pawn)
-	Mesh1P = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("CharacterMesh1P"));
-	Mesh1P->SetOnlyOwnerSee(true);
-	Mesh1P->SetupAttachment(FirstPersonCameraComponent);
-	Mesh1P->bCastDynamicShadow = false;
-	Mesh1P->CastShadow = false;
-	Mesh1P->SetRelativeRotation(FRotator(1.9f, -19.19f, 5.2f));
-	Mesh1P->SetRelativeLocation(FVector(-0.5f, -4.4f, -155.7f));
+    // Create a mesh component that will be used when being viewed from a '1st person' view (when controlling this pawn)
+    Mesh1P = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("CharacterMesh1P"));
+    Mesh1P->SetOnlyOwnerSee(true);
+    Mesh1P->SetupAttachment(FirstPersonCameraComponent);
+    Mesh1P->bCastDynamicShadow = false;
+    Mesh1P->CastShadow = false;
+    Mesh1P->SetRelativeRotation(FRotator(1.9f, -19.19f, 5.2f));
+    Mesh1P->SetRelativeLocation(FVector(-0.5f, -4.4f, -155.7f));
 
-	// Create a gun mesh component
-	FP_Gun = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("FP_Gun"));
-	FP_Gun->SetOnlyOwnerSee(false);			// otherwise won't be visible in the multiplayer
-	FP_Gun->bCastDynamicShadow = false;
-	FP_Gun->CastShadow = false;
-	// FP_Gun->SetupAttachment(Mesh1P, TEXT("GripPoint"));
-	FP_Gun->SetupAttachment(RootComponent);
+    // Create a gun mesh component
+    FP_Gun = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("FP_Gun"));
+    FP_Gun->SetOnlyOwnerSee(false);			// otherwise won't be visible in the multiplayer
+    FP_Gun->bCastDynamicShadow = false;
+    FP_Gun->CastShadow = false;
+    // FP_Gun->SetupAttachment(Mesh1P, TEXT("GripPoint"));
+    FP_Gun->SetupAttachment(RootComponent);
 
-	FP_MuzzleLocation = CreateDefaultSubobject<USceneComponent>(TEXT("MuzzleLocation"));
-	FP_MuzzleLocation->SetupAttachment(FP_Gun);
-	FP_MuzzleLocation->SetRelativeLocation(FVector(0.2f, 48.4f, -10.6f));
+    FP_MuzzleLocation = CreateDefaultSubobject<USceneComponent>(TEXT("MuzzleLocation"));
+    FP_MuzzleLocation->SetupAttachment(FP_Gun);
+    FP_MuzzleLocation->SetRelativeLocation(FVector(0.2f, 48.4f, -10.6f));
 
-	// Default offset from the character location for projectiles to spawn
-	GunOffset = FVector(100.0f, 0.0f, 10.0f);
+    // Default offset from the character location for projectiles to spawn
+    GunOffset = FVector(100.0f, 0.0f, 10.0f);
 
-	// Note: The ProjectileClass and the skeletal mesh/anim blueprints for Mesh1P, FP_Gun, and VR_Gun 
-	// are set in the derived blueprint asset named MyCharacter to avoid direct content references in C++.
+    // Note: The ProjectileClass and the skeletal mesh/anim blueprints for Mesh1P, FP_Gun, and VR_Gun 
+    // are set in the derived blueprint asset named MyCharacter to avoid direct content references in C++.
 
-	// Create VR Controllers.
-	R_MotionController = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("R_MotionController"));
-	R_MotionController->MotionSource = FXRMotionControllerBase::RightHandSourceId;
-	R_MotionController->SetupAttachment(RootComponent);
-	L_MotionController = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("L_MotionController"));
-	L_MotionController->SetupAttachment(RootComponent);
+    // Create VR Controllers.
+    R_MotionController = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("R_MotionController"));
+    R_MotionController->MotionSource = FXRMotionControllerBase::RightHandSourceId;
+    R_MotionController->SetupAttachment(RootComponent);
+    L_MotionController = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("L_MotionController"));
+    L_MotionController->SetupAttachment(RootComponent);
 
-	// Create a gun and attach it to the right-hand VR controller.
-	// Create a gun mesh component
-	VR_Gun = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("VR_Gun"));
-	VR_Gun->SetOnlyOwnerSee(false);			// otherwise won't be visible in the multiplayer
-	VR_Gun->bCastDynamicShadow = false;
-	VR_Gun->CastShadow = false;
-	VR_Gun->SetupAttachment(R_MotionController);
-	VR_Gun->SetRelativeRotation(FRotator(0.0f, -90.0f, 0.0f));
+    // Create a gun and attach it to the right-hand VR controller.
+    // Create a gun mesh component
+    VR_Gun = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("VR_Gun"));
+    VR_Gun->SetOnlyOwnerSee(false);			// otherwise won't be visible in the multiplayer
+    VR_Gun->bCastDynamicShadow = false;
+    VR_Gun->CastShadow = false;
+    VR_Gun->SetupAttachment(R_MotionController);
+    VR_Gun->SetRelativeRotation(FRotator(0.0f, -90.0f, 0.0f));
 
-	VR_MuzzleLocation = CreateDefaultSubobject<USceneComponent>(TEXT("VR_MuzzleLocation"));
-	VR_MuzzleLocation->SetupAttachment(VR_Gun);
-	VR_MuzzleLocation->SetRelativeLocation(FVector(0.000004, 53.999992, 10.000000));
-	VR_MuzzleLocation->SetRelativeRotation(FRotator(0.0f, 90.0f, 0.0f));		// Counteract the rotation of the VR gun model.
+    VR_MuzzleLocation = CreateDefaultSubobject<USceneComponent>(TEXT("VR_MuzzleLocation"));
+    VR_MuzzleLocation->SetupAttachment(VR_Gun);
+    VR_MuzzleLocation->SetRelativeLocation(FVector(0.000004, 53.999992, 10.000000));
+    VR_MuzzleLocation->SetRelativeRotation(FRotator(0.0f, 90.0f, 0.0f));		// Counteract the rotation of the VR gun model.
 
-	// Uncomment the following line to turn motion controllers on by default:
-	//bUsingMotionControllers = true;
+    // Uncomment the following line to turn motion controllers on by default:
+    //bUsingMotionControllers = true;
 }
 
-void ASphereAmbushCharacter::BeginPlay()
-{
-	// Call the base class  
-	Super::BeginPlay();
+void ASphereAmbushCharacter::BeginPlay() {
+    // Call the base class  
+    Super::BeginPlay();
 
-	//Attach gun mesh component to Skeleton, doing it here because the skeleton is not yet created in the constructor
-	FP_Gun->AttachToComponent(Mesh1P, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint"));
+    //Attach gun mesh component to Skeleton, doing it here because the skeleton is not yet created in the constructor
+    FP_Gun->AttachToComponent(Mesh1P, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint"));
 
-	// Show or hide the two versions of the gun based on whether or not we're using motion controllers.
-	if (bUsingMotionControllers)
-	{
-		VR_Gun->SetHiddenInGame(false, true);
-		Mesh1P->SetHiddenInGame(true, true);
-	}
-	else
-	{
-		VR_Gun->SetHiddenInGame(true, true);
-		Mesh1P->SetHiddenInGame(false, true);
-	}
+    // Show or hide the two versions of the gun based on whether or not we're using motion controllers.
+    if (bUsingMotionControllers) {
+        VR_Gun->SetHiddenInGame(false, true);
+        Mesh1P->SetHiddenInGame(true, true);
+    } else {
+        VR_Gun->SetHiddenInGame(true, true);
+        Mesh1P->SetHiddenInGame(false, true);
+    }
 }
 
 //////////////////////////////////////////////////////////////////////////
 // Input
 
-void ASphereAmbushCharacter::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent)
-{
-	// set up gameplay key bindings
-	check(PlayerInputComponent);
+void ASphereAmbushCharacter::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) {
+    // set up gameplay key bindings
+    check(PlayerInputComponent);
 
-	// Bind jump events
-	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
-	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
+    // Bind jump events
+    PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
+    PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
 
-	// Bind fire event
-	PlayerInputComponent->BindAction("Fire", IE_Pressed, this, &ASphereAmbushCharacter::OnFire);
+    // Bind fire event
+    PlayerInputComponent->BindAction("Fire", IE_Pressed, this, &ASphereAmbushCharacter::OnFire);
 
-	// Enable touchscreen input
-	EnableTouchscreenMovement(PlayerInputComponent);
+    // Enable touchscreen input
+    EnableTouchscreenMovement(PlayerInputComponent);
 
-	PlayerInputComponent->BindAction("ResetVR", IE_Pressed, this, &ASphereAmbushCharacter::OnResetVR);
+    PlayerInputComponent->BindAction("ResetVR", IE_Pressed, this, &ASphereAmbushCharacter::OnResetVR);
 
-	// Bind movement events
-	PlayerInputComponent->BindAxis("MoveForward", this, &ASphereAmbushCharacter::MoveForward);
-	PlayerInputComponent->BindAxis("MoveRight", this, &ASphereAmbushCharacter::MoveRight);
-
-	// We have 2 versions of the rotation bindings to handle different kinds of devices differently
-	// "turn" handles devices that provide an absolute delta, such as a mouse.
-	// "turnrate" is for devices that we choose to treat as a rate of change, such as an analog joystick
-	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
-	PlayerInputComponent->BindAxis("TurnRate", this, &ASphereAmbushCharacter::TurnAtRate);
-	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
-	PlayerInputComponent->BindAxis("LookUpRate", this, &ASphereAmbushCharacter::LookUpAtRate);
-}
-
-void ASphereAmbushCharacter::OnFire()
-{
-	// try and fire a projectile
-	if (ProjectileClass != nullptr)
-	{
-		UWorld* const World = GetWorld();
-		if (World != nullptr)
-		{
-			if (bUsingMotionControllers)
-			{
-				const FRotator SpawnRotation = VR_MuzzleLocation->GetComponentRotation();
-				const FVector SpawnLocation = VR_MuzzleLocation->GetComponentLocation();
-				World->SpawnActor<ASphereAmbushProjectile>(ProjectileClass, SpawnLocation, SpawnRotation);
-			}
-			else
-			{
-				const FRotator SpawnRotation = GetControlRotation();
-				// MuzzleOffset is in camera space, so transform it to world space before offsetting from the character location to find the final muzzle position
-				const FVector SpawnLocation = ((FP_MuzzleLocation != nullptr) ? FP_MuzzleLocation->GetComponentLocation() : GetActorLocation()) + SpawnRotation.RotateVector(GunOffset);
-
-				//Set Spawn Collision Handling Override
-				FActorSpawnParameters ActorSpawnParams;
-				ActorSpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButDontSpawnIfColliding;
-
-				// spawn the projectile at the muzzle
-				World->SpawnActor<ASphereAmbushProjectile>(ProjectileClass, SpawnLocation, SpawnRotation, ActorSpawnParams);
-			}
-		}
-	}
-
+    // Bind movement events
 #if 0
-	// try and play the sound if specified
-	if (FireSound != nullptr)
-	{
-		UGameplayStatics::PlaySoundAtLocation(this, FireSound, GetActorLocation());
-	}
+    PlayerInputComponent->BindAxis("MoveForward", this, &ASphereAmbushCharacter::MoveForward);
+    PlayerInputComponent->BindAxis("MoveRight", this, &ASphereAmbushCharacter::MoveRight);
 #endif
 
-	// try and play a firing animation if specified
-	if (FireAnimation != nullptr)
-	{
-		// Get the animation object for the arms mesh
-		UAnimInstance* AnimInstance = Mesh1P->GetAnimInstance();
-		if (AnimInstance != nullptr)
-		{
-			AnimInstance->Montage_Play(FireAnimation, 1.f);
-		}
-	}
+    // We have 2 versions of the rotation bindings to handle different kinds of devices differently
+    // "turn" handles devices that provide an absolute delta, such as a mouse.
+    // "turnrate" is for devices that we choose to treat as a rate of change, such as an analog joystick
+    PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+    PlayerInputComponent->BindAxis("TurnRate", this, &ASphereAmbushCharacter::TurnAtRate);
+    PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
+    PlayerInputComponent->BindAxis("LookUpRate", this, &ASphereAmbushCharacter::LookUpAtRate);
 }
 
-void ASphereAmbushCharacter::OnResetVR()
-{
-	UHeadMountedDisplayFunctionLibrary::ResetOrientationAndPosition();
+void ASphereAmbushCharacter::OnFire() {
+    // try and fire a projectile
+    if (ProjectileClass != nullptr) {
+        UWorld* const World = GetWorld();
+        if (World != nullptr) {
+            if (bUsingMotionControllers) {
+                const FRotator SpawnRotation = VR_MuzzleLocation->GetComponentRotation();
+                const FVector SpawnLocation = VR_MuzzleLocation->GetComponentLocation();
+                World->SpawnActor<ASphereAmbushProjectile>(ProjectileClass, SpawnLocation, SpawnRotation);
+            } else {
+                const FRotator SpawnRotation = GetControlRotation();
+                // MuzzleOffset is in camera space, so transform it to world space before offsetting from the character location to find the final muzzle position
+                const FVector SpawnLocation = ((FP_MuzzleLocation != nullptr) ? FP_MuzzleLocation->GetComponentLocation() : GetActorLocation()) + SpawnRotation.RotateVector(GunOffset);
+
+                //Set Spawn Collision Handling Override
+                FActorSpawnParameters ActorSpawnParams;
+                ActorSpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButDontSpawnIfColliding;
+
+                // spawn the projectile at the muzzle
+                World->SpawnActor<ASphereAmbushProjectile>(ProjectileClass, SpawnLocation, SpawnRotation, ActorSpawnParams);
+            }
+        }
+    }
+
+#if 0
+    // try and play the sound if specified
+    if (FireSound != nullptr) {
+        UGameplayStatics::PlaySoundAtLocation(this, FireSound, GetActorLocation());
+    }
+#endif
+
+    // try and play a firing animation if specified
+    if (FireAnimation != nullptr) {
+        // Get the animation object for the arms mesh
+        UAnimInstance* AnimInstance = Mesh1P->GetAnimInstance();
+        if (AnimInstance != nullptr) {
+            AnimInstance->Montage_Play(FireAnimation, 1.f);
+        }
+    }
 }
 
-void ASphereAmbushCharacter::BeginTouch(const ETouchIndex::Type FingerIndex, const FVector Location)
-{
-	if (TouchItem.bIsPressed == true)
-	{
-		return;
-	}
-	if ((FingerIndex == TouchItem.FingerIndex) && (TouchItem.bMoved == false))
-	{
-		OnFire();
-	}
-	TouchItem.bIsPressed = true;
-	TouchItem.FingerIndex = FingerIndex;
-	TouchItem.Location = Location;
-	TouchItem.bMoved = false;
+void ASphereAmbushCharacter::OnResetVR() {
+    UHeadMountedDisplayFunctionLibrary::ResetOrientationAndPosition();
 }
 
-void ASphereAmbushCharacter::EndTouch(const ETouchIndex::Type FingerIndex, const FVector Location)
-{
-	if (TouchItem.bIsPressed == false)
-	{
-		return;
-	}
-	TouchItem.bIsPressed = false;
+void ASphereAmbushCharacter::BeginTouch(const ETouchIndex::Type FingerIndex, const FVector Location) {
+    if (TouchItem.bIsPressed == true) {
+        return;
+    }
+    if ((FingerIndex == TouchItem.FingerIndex) && (TouchItem.bMoved == false)) {
+        OnFire();
+    }
+    TouchItem.bIsPressed = true;
+    TouchItem.FingerIndex = FingerIndex;
+    TouchItem.Location = Location;
+    TouchItem.bMoved = false;
+}
+
+void ASphereAmbushCharacter::EndTouch(const ETouchIndex::Type FingerIndex, const FVector Location) {
+    if (TouchItem.bIsPressed == false) {
+        return;
+    }
+    TouchItem.bIsPressed = false;
 }
 
 //Commenting this section out to be consistent with FPS BP template.
@@ -256,47 +237,40 @@ void ASphereAmbushCharacter::EndTouch(const ETouchIndex::Type FingerIndex, const
 //	}
 //}
 
-void ASphereAmbushCharacter::MoveForward(float Value)
-{
-	if (Value != 0.0f)
-	{
-		// add movement in that direction
-		AddMovementInput(GetActorForwardVector(), Value);
-	}
+void ASphereAmbushCharacter::MoveForward(float Value) {
+    if (Value != 0.0f) {
+        // add movement in that direction
+        AddMovementInput(GetActorForwardVector(), Value);
+    }
 }
 
-void ASphereAmbushCharacter::MoveRight(float Value)
-{
-	if (Value != 0.0f)
-	{
-		// add movement in that direction
-		AddMovementInput(GetActorRightVector(), Value);
-	}
+void ASphereAmbushCharacter::MoveRight(float Value) {
+    if (Value != 0.0f) {
+        // add movement in that direction
+        AddMovementInput(GetActorRightVector(), Value);
+    }
+
 }
 
-void ASphereAmbushCharacter::TurnAtRate(float Rate)
-{
-	// calculate delta for this frame from the rate information
-	AddControllerYawInput(Rate * BaseTurnRate * GetWorld()->GetDeltaSeconds());
+void ASphereAmbushCharacter::TurnAtRate(float Rate) {
+    // calculate delta for this frame from the rate information
+    AddControllerYawInput(Rate * BaseTurnRate * GetWorld()->GetDeltaSeconds());
 }
 
-void ASphereAmbushCharacter::LookUpAtRate(float Rate)
-{
-	// calculate delta for this frame from the rate information
-	AddControllerPitchInput(Rate * BaseLookUpRate * GetWorld()->GetDeltaSeconds());
+void ASphereAmbushCharacter::LookUpAtRate(float Rate) {
+    // calculate delta for this frame from the rate information
+    AddControllerPitchInput(Rate * BaseLookUpRate * GetWorld()->GetDeltaSeconds());
 }
 
-bool ASphereAmbushCharacter::EnableTouchscreenMovement(class UInputComponent* PlayerInputComponent)
-{
-	if (FPlatformMisc::SupportsTouchInput() || GetDefault<UInputSettings>()->bUseMouseForTouch)
-	{
-		PlayerInputComponent->BindTouch(EInputEvent::IE_Pressed, this, &ASphereAmbushCharacter::BeginTouch);
-		PlayerInputComponent->BindTouch(EInputEvent::IE_Released, this, &ASphereAmbushCharacter::EndTouch);
+bool ASphereAmbushCharacter::EnableTouchscreenMovement(class UInputComponent* PlayerInputComponent) {
+    if (FPlatformMisc::SupportsTouchInput() || GetDefault<UInputSettings>()->bUseMouseForTouch) {
+        PlayerInputComponent->BindTouch(EInputEvent::IE_Pressed, this, &ASphereAmbushCharacter::BeginTouch);
+        PlayerInputComponent->BindTouch(EInputEvent::IE_Released, this, &ASphereAmbushCharacter::EndTouch);
 
-		//Commenting this out to be more consistent with FPS BP template.
-		//PlayerInputComponent->BindTouch(EInputEvent::IE_Repeat, this, &ASphereAmbushCharacter::TouchUpdate);
-		return true;
-	}
-	
-	return false;
+        //Commenting this out to be more consistent with FPS BP template.
+        //PlayerInputComponent->BindTouch(EInputEvent::IE_Repeat, this, &ASphereAmbushCharacter::TouchUpdate);
+        return true;
+    }
+
+    return false;
 }
